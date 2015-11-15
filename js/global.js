a7e6cbe9b75df3a577ea84294487f9e12aa8832a
fengyuanzemin <fengyuanzemin@gmail.com>
2015-11-16 02:44:06 +0800

    emoji，有图片的链接，可以正常收数据

--- js/global.js
+++ js/global.js
@@ -2,64 +2,102 @@
 $(".prize").on('click', function(){
     location.assign("prizeB.html");
 });
-// 定时刷新页面	
-var page = $(".show-container");
+
+getData();
+// 定时获取数据
+function getData() {
+    setInterval(Ajax, 3000);
+}
 var time1;
-setInterval(function(){
-    // planB
-    // 收起
-    // $(".window").first().slideUp(700);
-	// 添加
-    clearInterval(time1);
-    $(".show-container").append(
-        "<div class='window'>" +
-        "<div class='information col-lg-3 col-md-3 col-sm-3 col-xs-3'>" +
-        "<div class='avatar'>" +
-        "<img class='img-responsive' src='img/2.jpg'>" +
-        "</div></div>" +
-        "<div class='show col-lg-8 col-md-8 col-sm-8 col-xs-8'>" +
-        "<h3><strong>沃德天·维神墨：</strong></h3>" +
-        "<h1>柔软舒适，格纹色彩均很满意，前胸后背均有仿羊羔绒保暖</h1>" +
-        "</div></div>"
-        );
-    // $(".window").last().hide();
-    // 定时删掉
-    // setTimeout(function(){
-    //     $('.window').first().remove();
-    //     $(".window").last().fadeIn(700);
-    // }, 705);
+// Ajax
+function Ajax() {
+    $.ajax({ 
+        type: "GET",    
+        url: "http://2.934067696.sinaapp.com/index.php",
+        dataType:'jsonp',  
+        data:'',  
+        jsonp:'callback',  
+        success:function(result) { 
+            console.log(result);
+            var page = $(".show-container");
+            setTimeout(function(){
+                clearInterval(time1);
+                $(".show-container").append(
+                    "<div class='window'>" +
+                    "<div class='information col-lg-3 col-md-3 col-sm-3 col-xs-3'>" +
+                    "<div class='avatar'>" +
+                    "<img class='img-responsive' src='" + result.avatar + "'>" +
+                    "</div></div>" +
+                    "<div class='show col-lg-8 col-md-8 col-sm-8 col-xs-8'>" +
+                    "<h3><strong>" + result.name + "</strong></h3>" +
+                    "<h1>" + result.words  + result.emoji + "</h1>" +
+                    "</div></div>"
+                    );
+                    time1 = setInterval(function(){
+                        page.scrollTop(page.scrollTop()+1);
+                    },8);
+            },1000);
+        },  
+        timeout:3000, //请求超时时间
+        error: function(jqXHR){     
+            console.log("发生错误：" + jqXHR.status);  
+        },     
+    });
+}
 
-    // planA
-    // setTimeout(function(){
+// roll();
+// 定时刷新页面，页面滚动	
+function roll() {
+    var page = $(".show-container");
+    var time1;
+    setInterval(function(){
+        clearInterval(time1);
+        $(".show-container").append(
+            "<div class='window'>" +
+            "<div class='information col-lg-3 col-md-3 col-sm-3 col-xs-3'>" +
+            "<div class='avatar'>" +
+            "<img class='img-responsive' src='img/2.jpg'>" +
+            "</div></div>" +
+            "<div class='show col-lg-8 col-md-8 col-sm-8 col-xs-8'>" +
+            "<h3><strong>沃德天·维神墨：</strong></h3>" +
+            "<h1>柔软舒适，格纹色彩均很满意，前胸后背均有仿羊羔绒保暖</h1>" +
+            "</div></div>"
+            );
+            time1 = setInterval(function(){
+                page.scrollTop(page.scrollTop()+1);
+            },8);
+    },6000);
+}
+// 添加emoji
+// twemoji.parse(
+//   'I \u2764\uFE0F emoji!',
+//   function(icon, options, variant) {
+//     return '/emoji/' + options.size + '/' + icon + '.gif';
+//   }
+// );
 
-    //     $(".window:first-child").remove();
-    // },500);
-    // $(".window").animate({ 
-    //     top: '-25%'
-    // },
-    // { 
-    //     easing: 'easeInOutQuad', 
-    //     duration: 500,
-    //     complete: function () {
-    //         $(".window").css('top','0');
-    //         // $(".window").eq(0).remove();
-    //     } 
-    // });
-    
-    
-        time1 = setInterval(function(){
-            // if(page.scrollTop() == page[0].scrollHeight) {
-            //     alert('a');
-                
-            // }
-            page.scrollTop(page.scrollTop()+1);
-        },1);
-        // setTimeout(function(){
-        //     $(".window:first-child").remove();
-        // },1500);
-        
-    // });
-},4000);
-
-
-
+// 没有用的代码
+// timeer();
+// function timeer() {
+//     var page = $(".show-container");
+//     var time1;
+//     clearInterval(time1);
+//     setTimeout(function(){
+//         clearInterval(time1);
+//         $(".show-container").append(
+//             "<div class='window'>" +
+//             "<div class='information col-lg-3 col-md-3 col-sm-3 col-xs-3'>" +
+//             "<div class='avatar'>" +
+//             "<img class='img-responsive' src='img/2.jpg'>" +
+//             "</div></div>" +
+//             "<div class='show col-lg-8 col-md-8 col-sm-8 col-xs-8'>" +
+//             "<h3><strong>沃德天·维神墨：</strong></h3>" +
+//             "<h1>柔软舒适，格纹色彩均很满意，前胸后背均有仿羊羔绒保暖</h1>" +
+//             "</div></div>"
+//             );
+//             time1 = setInterval(function(){
+//                 page.scrollTop(page.scrollTop()+1);
+//             },8);
+//         timeer();
+//     },4000);
+// }
