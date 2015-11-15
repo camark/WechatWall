19e3622fd7a62d0b3e8d752ed805b1ddd34015be
MaXueKai <422580963@qq.com>
2015-11-15 22:16:17 +0800

    微信墙滚动

--- js/global.js
+++ js/global.js
@@ -3,22 +3,25 @@ $(".prize").on('click', function(){
     location.assign("prizeB.html");
 });
 // 定时刷新页面	
+var page = $(".show-container");
+var time1;
 setInterval(function(){
     // planB
     // 收起
     // $(".window").first().slideUp(700);
 	// 添加
-    // $(".show-container").append(
-    //     "<div class='window'>" +
-    //     "<div class='information col-lg-3 col-md-3 col-sm-3 col-xs-3'>" +
-    //     "<div class='avatar'>" +
-    //     "<img class='img-responsive' src='img/2.jpg'>" +
-    //     "</div></div>" +
-    //     "<div class='show col-lg-8 col-md-8 col-sm-8 col-xs-8'>" +
-    //     "<h3><strong>沃德天·维神墨：</strong></h3>" +
-    //     "<h1>柔软舒适，格纹色彩均很满意，前胸后背均有仿羊羔绒保暖</h1>" +
-    //     "</div></div>"
-    //     );
+    clearInterval(time1);
+    $(".show-container").append(
+        "<div class='window'>" +
+        "<div class='information col-lg-3 col-md-3 col-sm-3 col-xs-3'>" +
+        "<div class='avatar'>" +
+        "<img class='img-responsive' src='img/2.jpg'>" +
+        "</div></div>" +
+        "<div class='show col-lg-8 col-md-8 col-sm-8 col-xs-8'>" +
+        "<h3><strong>沃德天·维神墨：</strong></h3>" +
+        "<h1>柔软舒适，格纹色彩均很满意，前胸后背均有仿羊羔绒保暖</h1>" +
+        "</div></div>"
+        );
     // $(".window").last().hide();
     // 定时删掉
     // setTimeout(function(){
@@ -27,11 +30,36 @@ setInterval(function(){
     // }, 705);
 
     // planA
-    $(".window").first().animate({ 
-        top: "-80px"
-    },{ 
-        easing: 'easeInOutQuad', 
-        duration: 500, 
-        // complete: callback 
-    }); 
-},3000);
+    // setTimeout(function(){
+
+    //     $(".window:first-child").remove();
+    // },500);
+    // $(".window").animate({ 
+    //     top: '-25%'
+    // },
+    // { 
+    //     easing: 'easeInOutQuad', 
+    //     duration: 500,
+    //     complete: function () {
+    //         $(".window").css('top','0');
+    //         // $(".window").eq(0).remove();
+    //     } 
+    // });
+    
+    
+        time1 = setInterval(function(){
+            // if(page.scrollTop() == page[0].scrollHeight) {
+            //     alert('a');
+                
+            // }
+            page.scrollTop(page.scrollTop()+1);
+        },1);
+        // setTimeout(function(){
+        //     $(".window:first-child").remove();
+        // },1500);
+        
+    // });
+},4000);
+
+
+
