6542b473ea9be6e7f766258c673f1e72899ef458
MaXueKai <422580963@qq.com>
2015-11-15 15:48:00 +0800

    抽奖plan_B

--- js/global.js
+++ js/global.js
@@ -1,5 +1,7 @@
 // 点击抽奖跳转
 $(".prize").on('click', function(){
     location.assign("prize.html");
+    // $('.window').eq(0).remove();
+    // $(".show-container").append("<div class='window'><div class='information col-lg-3 col-md-3 col-sm-3 col-xs-3'><div class='avatar'><img class='img-responsive' src='img/2.jpg'></div></div><div class='show col-lg-8 col-md-8 col-sm-8 col-xs-8'><h3><strong>沃德天·维神墨：</strong></h3><h1>柔软舒适，格纹色彩均很满意，前胸后背均有仿羊羔绒保暖</h1></div></div>");
 });
 	
