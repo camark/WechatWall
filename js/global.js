94e7ed909a8cb3313dc7e011695476bb156e4481
fengyuanzemin <fengyuanzemin@gmail.com>
2015-11-16 12:15:29 +0800

    将图片存入本地

--- js/global.js
+++ js/global.js
@@ -30,7 +30,7 @@ function Ajax() {
                     "</div></div>" +
                     "<div class='show col-lg-9 col-md-9 col-sm-9 col-xs-9'>" +
                     "<h3><strong>" + result.name + "</strong></h3>" +
-                    "<h1>" + result.words  + result.emoji + "</h1>" +
+                    "<h1>" + result.words  + "</h1>" +
                     "</div></div>"
                     );
                     time1 = setInterval(function(){
