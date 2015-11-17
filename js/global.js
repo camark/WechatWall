79d194fde7a85458a547a9e5245fd3b3b45c3bcd
fengyuanzemin <fengyuanzemin@gmail.com>
2015-11-17 17:16:24 +0800

    晚会前

--- js/global.js
+++ js/global.js
@@ -70,7 +70,7 @@ function Ajax() {
                 "</div></div>" +
                 "<div class='show col-lg-9 col-md-9 col-sm-9 col-xs-9'>" +
                 "<h3><strong>" + result.msg[i].NickName + "</strong></h3>" +
-                "<h1>" + result.msg[i].Content  + "</h1>" +
+                "<h1>" + result.msg[i].Content.split('#')[1]  + "</h1>" +
                 "</div></div>"
                 );
             }  
