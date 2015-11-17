79082bd02ddbf379340fc8d2bce421a613f56029
fengyuanzemin <fengyuanzemin@gmail.com>
2015-11-17 14:01:53 +0800

    数组添加bug

--- js/sift.js
+++ js/sift.js
@@ -1,29 +1,26 @@
-var time1, getTimer , count = 0;
-// id = new Array();
-// nickName = new Array();
-// avatar = new Array();
-// content = new Array();
-// msgId = new Array();
-var data = []
+var time1, getTimer , count = 0, msg;
+var data = [];
 
 // 页面加载完后触发
-$(function(){
-    getData();
-    $(window).on('blur',function(){
-        clearInterval(time1);
-        clearInterval(getTimer);
-    });
-    $(window).on('focus',function(){
-        getData();
-    });   
-})
+// $(function(){
+//     getData();
+//     $(window).on('blur',function(){
+//         clearInterval(time1);
+//         clearInterval(getTimer);
+//     });
+//     $(window).on('focus',function(){
+//         getData();
+//     });   
+// })
+
+setInterval(getData, 1000);
 // 定时获取数据
 function getData() {
     
     if(count == 0) {
         $.ajax({ 
             type: "GET",    
-            url: "http://115.28.240.51/wechat/msglist.php?Msg=" + count,
+            url: "http://115.28.240.51/wechat/msglist.php?msgid=" + count,
             dataType:'json',  
             data:'',    
             success:function(result) { 
@@ -33,101 +30,116 @@ function getData() {
                 }
                 count ++;
                 console.log(result);
-                // id.push(result.id);
-                data.push({
-                    nickName： result.nickName，
-                    avatar:    result.avatar,
-                    content:   result.words
+                result.msg.forEach(function(msg) {
+                    data.push({
+                        nickName:  msg.NickName,
+                        fakeId:    msg.FakeId,
+                        content:   msg.Content,
+                        msgId:     msg.MsgId,
+                    })
                 })
-                // nickName.push(result.nickName);
-                // avatar.push(result.avatar);
-                // content.push(result.words); 
-                Msg = result.msgId;
-                msgId.push(result.msgId);
-                console.log(data[data.length-1])
-                // console.log(name1);
-                // console.log(avatar);
-                // console.log(content);
-
+                msg = result.msg[0].MsgId;
+                // console.log(msg);
+                // console.log(data[data.length-1]);
             },  
             timeout:3000, //请求超时时间
             error: function(jqXHR){     
-                gerData();
+                getData();
                 console.log("发生错误：" + jqXHR.status);  
             },     
         });
     } else if(count > 0) {
-        setInterval(Ajax(Msg), 3000);
+        // console.log(count);
+        Ajax();
     }
 }
 
 // Ajax
-function Ajax(Msg) {
+function Ajax() {
     $.ajax({ 
         type: "GET",    
-        url: "http://115.28.240.51/wechat/msglist.php?msgid=" + Msg,
+        url: "http://115.28.240.51/wechat/msglist.php?msgid=" + msg,
         dataType:'json',  
         data:'',  
         success:function(result) { 
-            console.log(result);
-            // nickName.push(result.name);
-            // avatar.push(result.avatar);
-            // content.push(result.words);
-            // msgId.push(result.msgid);
-            data.push({
-                nickName: result.name,
-                avatar:   result.avatar,
-                content:  result.words,
-                msgId:    result.msgid
-            })
-            Msg = result.msgId;
-            // console.log(nickName);
-            // console.log(avatar);
-            // console.log(content); 
-            // console.log(msgId);   
+            if(result.num == 0) {
+                // Ajax();
+                return ;
+            }
+            // console.log(result);
+            result.msg.forEach( function(msg) {
+                    data.push({
+                        nickName:  msg.NickName,
+                        fakeId:    msg.FakeId,
+                        content:   msg.Content,
+                        msgId:     msg.MsgId,
+                    });
+                });
+            msg = data[result.num - 1].msgId;
+            // console.log(msg);
         },  
-        timeout:3000, //请求超时时间
+        //timeout:3000, //请求超时时间
         error: function(jqXHR){     
             console.log("发生错误：" + jqXHR.status);  
         },     
     });
 }
 
-i = -1;
-// roll();
+// setTimeout(function(){
+roll();
+// }, 1100);
+    
 // 定时刷新页面，页面滚动	
 function roll() {
-    i++;
+    i = 0;
+    // console.log(data);
+    // console.log(data.length);
+    // if(i > data.length) {
+    //     return ;
+    // }
+    // if(data.length == 0) {
+    //     i++;
+    // }
+    // if(data.length > 0) {
     var page = $(".show-container");
-    setTimeout(function(){
+    _time = setInterval(function(){
         clearInterval(time1);
+        console.log(data);
+        console.log(data.num);
         // var top = document.getComp
         $(".show-container").append(
             "<div class='window'>" +
             "<div class='information col-lg-2 col-md-2 col-sm-2 col-xs-2'>" +
-            "<img class='img-responsive' src='" + data[i].avatar + "'>" +
+            "<img class='img-responsive' src='http://115.28.240.51/wechat/img/" + data[i].fakeId + ".jpg'>" +
             "</div>" +
             "<div class='show col-lg-8 col-md-8 col-sm-8 col-xs-8'>" +
             "<h3><strong>" + data[i].nickName + "</strong></h3>" +
             "<p>" + data[i].content  + "</p></div>" +
             "<div class='wall col-lg-2 col-md-2 col-sm-2 col-xs-2'>" +
-            "<div title='" + data[i].msgid + "' class='btn btn-primary'>上墙</div>"+
+            "<div title='" + data[i].msgId + "' class='btn btn-primary'>上墙</div>"+
             "</div></div>"
             );
-        time1 = setInterval(function(){
+        time1 = setInterval(function() {
             page.scrollTop(page.scrollTop()+1);
         },8);  
-        roll();
+        // roll();
+        i++;
+        if(i >= data.num) {
+            i--;
+            console.log(11111);
+            clearInterval(_time);
+            setTimeout(roll, 6000);
+        }
     }, 5000);
-
+    // }
 }
 
 // 点击按钮，上墙
 $(".btn").on("click", function(){
-    // alert("1");
+    alert("1");
     $.ajax({ 
         type: "GET",    
-        url: "http://115.28.240.51/wechat/push.php?" + $(this).attr("title"),
+        url: "http://115.28.240.51/wechat/push.php?msgid=" + $(this).attr("title"),
         data:'',  
         success:function(result) { 
             alert("上墙成功");    
