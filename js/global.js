700fd3eb348e2df8ef78e80cc48a23343f860f2f
fengyuanzemin <fengyuanzemin@gmail.com>
2015-11-17 09:11:41 +0800

    昨晚

--- js/global.js
+++ js/global.js
@@ -73,31 +73,6 @@ function Ajax() {
     });
 }
 
-
-
-// roll();
-// 定时刷新页面，页面滚动	
-// function roll() {
-//     var page = $(".show-container");
-//     var time1;
-//     setInterval(function(){
-//         clearInterval(time1);
-//         $(".show-container").append(
-//             "<div class='window'>" +
-//             "<div class='information col-lg-3 col-md-3 col-sm-3 col-xs-3'>" +
-//             "<div class='avatar'>" +
-//             "<img class='img-responsive' src='img/2.jpg'>" +
-//             "</div></div>" +
-//             "<div class='show col-lg-8 col-md-8 col-sm-8 col-xs-8'>" +
-//             "<h3><strong>沃德天·维神墨：</strong></h3>" +
-//             "<h1>柔软舒适，格纹色彩均很满意，前胸后背均有仿羊羔绒保暖</h1>" +
-//             "</div></div>"
-//             );
-//             time1 = setInterval(function(){
-//                 page.scrollTop(page.scrollTop()+1);
-//             },8);
-//     },6000);
-// }
 // 添加emoji
 // twemoji.parse(
 //   'I \u2764\uFE0F emoji!',
@@ -105,29 +80,3 @@ function Ajax() {
 //     return '/emoji/' + options.size + '/' + icon + '.gif';
 //   }
 // );
-
-// 没有用的代码
-// timeer();
-// function timeer() {
-//     var page = $(".show-container");
-//     var time1;
-//     clearInterval(time1);
-//     setTimeout(function(){
-//         clearInterval(time1);
-//         $(".show-container").append(
-//             "<div class='window'>" +
-//             "<div class='information col-lg-3 col-md-3 col-sm-3 col-xs-3'>" +
-//             "<div class='avatar'>" +
-//             "<img class='img-responsive' src='img/2.jpg'>" +
-//             "</div></div>" +
-//             "<div class='show col-lg-8 col-md-8 col-sm-8 col-xs-8'>" +
-//             "<h3><strong>沃德天·维神墨：</strong></h3>" +
-//             "<h1>柔软舒适，格纹色彩均很满意，前胸后背均有仿羊羔绒保暖</h1>" +
-//             "</div></div>"
-//             );
-//             time1 = setInterval(function(){
-//                 page.scrollTop(page.scrollTop()+1);
-//             },8);
-//         timeer();
-//     },4000);
-// }
--- js/sift.js
+++ js/sift.js
@@ -1,4 +1,9 @@
-var time1,getTimer;
+var time1, getTimer;
+// id = new Array();
+name1 = new Array();
+avatar = new Array();
+content = new Array();
+
 // 页面加载完后触发
 $(function(){
     getData();
@@ -12,40 +17,27 @@ $(function(){
 })
 // 定时获取数据
 function getData() {
-    setInterval(Ajax, 3000);
+    // setInterval(Ajax, 3000);
 }
 
 // Ajax
 function Ajax() {
     $.ajax({ 
         type: "GET",    
-        url: "http://2.934067696.sinaapp.com/index.php",
-        dataType:'jsonp',  
+        url: "http://115.28.240.51/wechat/push.php",
+        // url: "http://2.934067696.sinaapp.com",
+        dataType:'json',  
         data:'',  
-        jsonp:'callback',  
+        // jsonp:'callback',  
         success:function(result) { 
             console.log(result);
-            var page = $(".show-container");
-            // setTimeout(function() {
-                clearInterval(time1);
-                $(".show-container").append(
-                    "<div class='window'>" +
-                    "<div class='information col-lg-2 col-md-2 col-sm-2 col-xs-2'>" +
-                    "<img class='img-responsive' src='" + result.avatar + "'>" +
-                    "</div>" +
-                    "<div class='show col-lg-8 col-md-8 col-sm-8 col-xs-8'>" +
-                    "<h3><strong>" + result.name + "</strong></h3>" +
-                    "<p>" + result.words  + "</p></div>" +
-                    "<div class='wall col-lg-2 col-md-2 col-sm-2 col-xs-2'>" +
-                    "<div class='btn btn-primary'>上墙</div>"+
-                    "</div></div>"
-                    );
-                // $(".window").first().remove();
-                    time1 = setInterval(function(){
-                        page.scrollTop(page.scrollTop()+1);
-                    },8);
-            // },3000);
-            
+            // id.push(result.id);
+            name1.push(result.name);
+            avatar.push(result.avatar);
+            content.push(result.words); 
+            console.log(name1);
+            console.log(avatar);
+            console.log(content);      
         },  
         timeout:3000, //请求超时时间
         error: function(jqXHR){     
@@ -54,29 +46,52 @@ function Ajax() {
     });
 }
 
+i = -1;
 // roll();
 // 定时刷新页面，页面滚动	
 function roll() {
+    i++;
     var page = $(".show-container");
-    var time1;
-    setInterval(function(){
+    setTimeout(function(){
         clearInterval(time1);
         $(".show-container").append(
             "<div class='window'>" +
-            "<div class='information col-lg-3 col-md-3 col-sm-3 col-xs-3'>" +
-            "<div class='avatar'>" +
-            "<img class='img-responsive' src='img/2.jpg'>" +
-            "</div></div>" +
+            "<div class='information col-lg-2 col-md-2 col-sm-2 col-xs-2'>" +
+            "<img class='img-responsive' src='" + avatar[i] + "'>" +
+            "</div>" +
             "<div class='show col-lg-8 col-md-8 col-sm-8 col-xs-8'>" +
-            "<h3><strong>沃德天·维神墨：</strong></h3>" +
-            "<h1>柔软舒适，格纹色彩均很满意，前胸后背均有仿羊羔绒保暖</h1>" +
+            "<h3><strong>" + name1[i] + "</strong></h3>" +
+            "<p>" + content[i]  + "</p></div>" +
+            "<div class='wall col-lg-2 col-md-2 col-sm-2 col-xs-2'>" +
+            "<div title='" + + "' class='btn btn-primary'>上墙</div>"+
             "</div></div>"
             );
-            time1 = setInterval(function(){
-                page.scrollTop(page.scrollTop()+1);
-            },8);
-    },6000);
+        time1 = setInterval(function(){
+            page.scrollTop(page.scrollTop()+1);
+        },8);  
+        roll();
+    }, 5000);
+
 }
+
+// 点击按钮，上墙
+$(".btn").on("click", function(){
+    // alert("1");
+    $.ajax({ 
+        type: "GET",    
+        url: "http://115.28.240.51/?" + $(this).attr("title"),
+        dataType:'jsonp',  
+        data:'',  
+        jsonp:'callback',  
+        success:function(result) { 
+            alert("上墙成功");    
+        },  
+        timeout:3000, //请求超时时间
+        error: function(jqXHR){     
+            console.log("发生错误：" + jqXHR.status);  
+        },     
+    });
+});
 // 添加emoji
 // twemoji.parse(
 //   'I \u2764\uFE0F emoji!',
@@ -84,29 +99,3 @@ function roll() {
 //     return '/emoji/' + options.size + '/' + icon + '.gif';
 //   }
 // );
-
-// 没有用的代码
-// timeer();
-// function timeer() {
-//     var page = $(".show-container");
-//     var time1;
-//     clearInterval(time1);
-//     setTimeout(function(){
-//         clearInterval(time1);
-//         $(".show-container").append(
-//             "<div class='window'>" +
-//             "<div class='information col-lg-3 col-md-3 col-sm-3 col-xs-3'>" +
-//             "<div class='avatar'>" +
-//             "<img class='img-responsive' src='img/2.jpg'>" +
-//             "</div></div>" +
-//             "<div class='show col-lg-8 col-md-8 col-sm-8 col-xs-8'>" +
-//             "<h3><strong>沃德天·维神墨：</strong></h3>" +
-//             "<h1>柔软舒适，格纹色彩均很满意，前胸后背均有仿羊羔绒保暖</h1>" +
-//             "</div></div>"
-//             );
-//             time1 = setInterval(function(){
-//                 page.scrollTop(page.scrollTop()+1);
-//             },8);
-//         timeer();
-//     },4000);
-// }
